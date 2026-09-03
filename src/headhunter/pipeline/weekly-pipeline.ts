b24c73031124b73af075cad097f0916c7b4333b3
1 Sunflower <1sunflower@1deMac-mini.local>
2026-09-03 16:01:56 +0800

    fix(finance): reject generic registry domains

--- src/headhunter/pipeline/weekly-pipeline.ts
+++ src/headhunter/pipeline/weekly-pipeline.ts
@@ -33,9 +33,9 @@ const NON_COMPANY_HOSTS = new Set([
   "facebook.com", "instagram.com", "youtube.com", "scmp.com", "chinadaily.com.cn", "chinadailyhk.com",
   "newsgd.com", "globaltimes.cn", "nytimes.com", "substack.com", "china-briefing.com", "vietnam-briefing.com",
   "harris-sliwoski.com", "woodburnglobal.com", "bridgepointgroup.com", "go-gba.com", "ourchinastory.com",
-  "hktdc.com", "nih.gov", "uscc.gov", "hotjob.cn", "iguopin.com", "seek.com", "naukri.com",
+  "hktdc.com", "nih.gov", "uscc.gov", "gov.cn", "org.cn", "edu.cn", "com.my", "hotjob.cn", "iguopin.com", "seek.com", "naukri.com",
   "pageexecutive.com", "valueaddvc.com", "brunel.com.cn", "hiredchina.com", "chinajob.com", "vietchina.org",
-  "michaelpage.com.cn", "robertwalters.cn", "arc-group.com", "gba-group-pharma.com"
+  "michaelpage.com.cn", "robertwalters.cn", "arc-group.com", "gba-group-pharma.com", "chinaglobalsouth.com"
 ]);
 const GENERIC_TITLE = /^(?:\d+\s+)?(?:finance|human resources|hr|recruiter|recruitment|jobs?|careers?|hiring|job openings?|search results?)(?:\s+(?:jobs?|in|hong kong|china|asia).*)?$/i;
 const SIGNAL_WORDS = /hiring|recruit|招聘|扩张|expansion|factory|plant|产线|融资|funding|ipo|acquisition|并购|license|牌照|treasury|总部|headquarters|海外|overseas|capacity|订单|order|management hire|重组|restructur/i;
